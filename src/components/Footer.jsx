82785e972f35c83d929c5423f6b289c8f35223ea
Dhruv Tiwari <167321903+whyweexist@users.noreply.github.com>
2024-10-15 07:10:01 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,12 +6,12 @@ const Footer = ({ userAddress }) => {
       <div className="container flex flex-col items-center justify-between p-6 mx-auto space-y-4 sm:space-y-0 sm:flex-row">
 
         <p className="text-sm text-gray-600 dark:text-gray-300">
-          Copyright © 2023 Quicklance. All Rights Reserved.
+          Copyright © 2024 Quicklance. All Rights Reserved.
         </p>
 
         <div className="flex -mx-2">
           <a
-            href="https://github.com/aniruddhafb/quicklance_ethforall"
+            href="https://github.com/whyweexist/lancer"
             target="_blank" rel="noreferrer"
             className="mx-2 text-gray-600 transition-colors duration-300 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400"
             aria-label="Github"
